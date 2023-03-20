945b51bd607584d84d472be5e31148021959ce2e
emilio <jemilio9606@gmail.com>
2023-03-19 23:00:13 -0400

    5.1. Añadida generic view para controlar los datos a mostrar

--- backend/warehouse/warehouseApp/url.py
+++ backend/warehouse/warehouseApp/url.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('list/', views.list),
+    # path('list/', views.list), # URL para vista mediante metodo
+    path('list/', views.listGeneric.as_view()) # URL para vista creada mediante vista generica.
+    # En este caso es necesario poner al final de la llamada a la vista el método as_view
 ]
--- backend/warehouse/warehouseApp/views.py
+++ backend/warehouse/warehouseApp/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from .models import Brand
-
-
-
-
+from django.views import generic
 def index(request):
     return render(request, 'layout.html')
 
 
 createIt = False
+# Listar las marcas mediante un metodo
 def list(request):
     if createIt:
         Brand.objects.create(name="Adidas")
@@ -20,7 +18,20 @@ def list(request):
         'name': 'Eduardo',
         'brands': brand
     }
+
+    # En este caso el nombre de la plantilla comienza con la carpeta donde está ubicada
     return render(request, 'pages/list.html', context)
 
+# Listar las marcas utilizando las vistas genericas
+class listGeneric(generic.ListView):
+    template_name = 'pages/list.html' # nombre de la plantilla que va a utilizar.
+    context_object_name = 'brands' # el nombre con el que se accederá al objeto principal en la plantilla
+    model = Brand # modelo que se va a utilizar para mostrar los datos
+
+    # Las vistas genéricas poseen una serie de métodos que podemos sobreescribir
+    def get_context_data(self, *, object_list=None, **kwargs): # Este método nos permite modificar los valores del contexto
+        context = super().get_context_data(**kwargs) # Creamos la variable contexto y le asignamos todos los datos que ya existían en la clase padre (brand.objects.all)
+        context['name']='Bruno' # añado un valor nuevo al contexto
+        return context # retorno el contexto
 def create(request):
     return render(request, 'pages/create.html')
