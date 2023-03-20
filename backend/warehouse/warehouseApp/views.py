c6bf72f52e7a7e7760dcd83f6229acd59e018292
emilio <jemilio9606@gmail.com>
2023-03-19 22:35:18 -0400

    5. Se modifico la vista list donde obtenemos los datos del objeto brand de la base de datos. Utilizamos el bloque for para mostrar en una tabla todas las Brand (marcas) guardadas en base de datos. Se añadió un bloque if para trabajar con las condicionales

--- backend/warehouse/warehouseApp/views.py
+++ backend/warehouse/warehouseApp/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from .models import Brand
 
 
-# Create your views here.
+
+
 def index(request):
     return render(request, 'layout.html')
 
 
+createIt = False
 def list(request):
-    return render(request, 'pages/list.html')
-
+    if createIt:
+        Brand.objects.create(name="Adidas")
+        Brand.objects.create(name="Nike")
+        Brand.objects.create(name="Converse")
+        Brand.objects.create(name="Vans")
+    brand = Brand.objects.all();
+    context = {
+        'name': 'Eduardo',
+        'brands': brand
+    }
+    return render(request, 'pages/list.html', context)
 
 def create(request):
     return render(request, 'pages/create.html')
