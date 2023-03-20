66445c3d77000a34706b8c774b80275eb184df6a
emilio <jemilio9606@gmail.com>
2023-03-19 20:50:44 -0400

    4. Add simple layout and two pages for tutorial example

--- backend/warehouse/warehouseApp/views.py
+++ backend/warehouse/warehouseApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'layout.html')
+
+
+def list(request):
+    return render(request, 'pages/list.html')
+
+
+def create(request):
+    return render(request, 'pages/create.html')
